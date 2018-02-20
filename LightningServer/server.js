223b7dd6d258a4498947a571ef0caf2d77587ba2
callidex <paul@callidex.com>
2018-02-20 21:01:20 +1000

    added test client, working backend database

--- LightningServer/server.js
+++ LightningServer/server.js
@@ -3,6 +3,7 @@ var HOST = '127.0.0.1';
 
 var dgram = require('dgram');
 var server = dgram.createSocket('udp4');
+var Datastore = require('nedb') , db = new Datastore({ filename: 'datastore.db', autoload: true })
 
 server.on('listening', function () {
    var address = server.address();
@@ -11,6 +12,18 @@ server.on('listening', function () {
 
 server.on('message', function (message, remote) {
    console.log(remote.address + ':' + remote.port + ' - ' + message);
+
+   var packet = {
+       type: 'data'
+      , data: message
+      , address: remote.address
+      , port: remote.port
+    
+   };
+
+   db.insert(packet, function (err, newDoc) {   // Callback is optional
+      // newDoc is the newly inserted document, including its _id
+   });
 
 });
 
