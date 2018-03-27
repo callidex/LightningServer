672c0596790dc955e9f0dd8e71758d707499f5e4
Paul Beet <paul@callidex.com>
2018-03-27 22:12:27 +1100

    filter to last 20 packets

--- LightningServer/api/controllers/lightningController.js
+++ LightningServer/api/controllers/lightningController.js
@@ -5,7 +5,9 @@ var Datastore = require("nedb");
 exports.list_all_packets = function(req, res) 
 {
    var db = new Datastore({ filename: "../../datastore.db", autoload: true });
-   db.find({}, function (err, docs) 
+
+   
+   db.find({}).sort( { timestamp: -1}).limit(20).exec(function (err, docs) 
       {
           if (err)  res.send(err);
     res.json(docs);    
