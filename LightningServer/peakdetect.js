ef08d0b5261f7d15dd97fc42254bed2b1e2adf45
callidex <paul@callidex.com>
2018-04-04 13:53:46 +1000

    rtfm

--- LightningServer/peakdetect.js
+++ LightningServer/peakdetect.js
@@ -2,27 +2,25 @@
 var math = require('mathjs');
 module.exports = {
     calcpeak: function (buffer, lag, threshold, inf) {
+
+        var stddev = math.std(buffer);
         var signal = [];
         for (var i = 0; i < buffer.length; i++) {
-            var laglhs = math.max(0, i - lag);
-            var lagrhs = i;
             if (i <= lag) {
                 signal.push(0);
             }
-
             else {
-                var window = buffer.slice(laglhs, i - laglhs);
+                var window = buffer.slice(i -lag, i);
                 if (window.length > 0) {
                     var windowavg = math.mean(window);
                     var windowstddev = math.std(window);
-                    if (windowstddev > threshold) {
+                    if (windowstddev > stddev) {
                         signal.push(1);
                     }
                     else { signal.push(0); }
                 } else { signal.push(0); }
             }
         }
-
         return signal;
     }
 }
