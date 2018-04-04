28046ba8014c15c53158899d476d64906984b8b0
Paul Beet <paul@callidex.com>
2018-04-04 13:32:03 +1000

    server side tweaks

--- LightningServer/mapping.js
+++ LightningServer/mapping.js
@@ -1,52 +1,57 @@
-﻿var Datastore = require("nedb");
+'use strict';
+var Datastore = require("nedb");
 var parser = require("./parse");
 var rethink = require('rethinkdb');
 
 function sleep(time) {
-   var stop = new Date().getTime();
-   while (new Date().getTime() < stop + time) {
-      ;
-   }
+    var stop = new Date().getTime();
+    while (new Date().getTime() < stop + time) {
+        ;
+    }
 }
 
 rethink.connect({ host: 's7.slashdit.com', port: 28015 }, function (err, conn) {
-   if (err) throw err;
-   conn.use('lightning');
+    if (err) throw err;
+    conn.use('lightning');
+
+    rethink.table('rawpackets').
+        orderBy(rethink.desc('received')).run(conn, function (err, cursor) {
+            if (err) throw err;
+            cursor.toArray().then(function (results) {
+                for (var i = 0; i < results.length; i++) {
+
+                    try {
 
 
+                        console.log('parsing object ', i);
+                        var parsedObject = parser.parseDataChunk(results[i]);
+                        if (parsedObject.batchid > 0) {
+                            if (parsedObject == null) {
+                                console.log("Unknown object");
+                            }
+                            else {
 
-   rethink.table('rawpackets').
-      orderBy(rethink.desc('received')).limit(2000).run(conn, function (err, cursor) {
-         if (err) throw err;
-         cursor.toArray().then(function (results) {
-            conn.close();
-            for (var i = 0; i < results.length; i++) {
+                                if (parsedObject) {
+                                    parsedObject.persistedDate = Date.now();
+                                    if (parsedObject.packettype != "sample" && parsedObject.packettype != 0) {
 
-               console.log('parsing object ', i);
-               var parsedObject = parser.parseDataChunk(results[i]);
-               if (parsedObject.batchid > 0) {
-                  if (parsedObject == null) {
-                     console.log("Unknown object");
-                  }
-                  else {
-
-                     if (parsedObject) {
-                        parsedObject.persistedDate = Date.now();
-                        if (parsedObject.packettype != "sample" && parsedObject.packettype != 0) {
-
-                           //      rethink.db('lightning').table('statuspackets').insert(parsedObject).run(conn, function(err, res) { if(err) throw err; });
+                                       rethink.db('lightning').table('statuspackets').insert(parsedObject).run(conn, function (err, res) { if (err) throw err; });
+                                    }
+                                    else {
+                                        if (parsedObject.maxval < 4096) {
+                                             rethink.db('lightning').table('datapackets').insert(parsedObject).run(conn, function (err, res) { if (err) throw err; });
+                                        }
+                                    }
+                                }
+                            }
                         }
-                        else {
-                           if (parsedObject.maxval < 4096) { }
 
-                           //       rethink.db('lightning').table('datapackets').insert(parsedObject).run(conn, function (err, res) { if (err) throw err; });              
-                        }
-                     }
-                  }
-
-               }
-            }
-         });
-         conn.close();
-      });
+                    }
+                    catch (e) {
+                        console.log(parsedObject);
+                        throw e;
+                    }
+                }
+            });
+        });
 });
--- LightningServer/peakdetect.js
+++ LightningServer/peakdetect.js
@@ -1,26 +1,28 @@
+'use strict';
 var math = require('mathjs');
 module.exports = {
-   calcpeak: function (buffer, lag, threshold, inf) {
-      var signal = [];
-      //move through buffer, looking last lag
-      var laglhs
-      for (var i = 0; i < buffer.length; i++) {
-         laglhs = math.max(0, i - lag);
-         var lagrhs = i;
-         if (i < lag) {
-            signal.push(0);
-         }
+    calcpeak: function (buffer, lag, threshold, inf) {
+        var signal = [];
+        for (var i = 0; i < buffer.length; i++) {
+            var laglhs = math.max(0, i - lag);
+            var lagrhs = i;
+            if (i <= lag) {
+                signal.push(0);
+            }
 
-         else {
-            var window = buffer.splice(laglhs, i - laglhs);
-            var windowavg = math.mean(window);
-            var windowstddev = math.std(window);
-            if (windowstddev > threshold) {
-               signal.push(1);
-            } else { signal.push(0); }
-         }
-      }
+            else {
+                var window = buffer.slice(laglhs, i - laglhs);
+                if (window.length > 0) {
+                    var windowavg = math.mean(window);
+                    var windowstddev = math.std(window);
+                    if (windowstddev > threshold) {
+                        signal.push(1);
+                    }
+                    else { signal.push(0); }
+                } else { signal.push(0); }
+            }
+        }
 
-      return signal;
-   }
+        return signal;
+    }
 }
