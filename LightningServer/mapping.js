01fe0f4c7de647869496f242735eea42518aeae5
callidex <paul@callidex.com>
2018-04-11 17:50:55 +1000

    move to mysql

--- LightningServer/mapping.js
+++ LightningServer/mapping.js
@@ -10,53 +10,130 @@ function sleep(time) {
     }
 }
 
-rethink.connect({ host: 's7.slashdit.com', port: 28015 }, function (err, conn) {
+const mysql = require('mysql2');
+
+var con = mysql.createConnection({
+    host: "s7.slashdit.com",
+    user: "mapping",
+    password: "mappingpwd"
+});
+
+con.connect(function (err) {
+
     if (err) throw err;
-    conn.use('lightning');
+    console.log("Connected!");
 
-    rethink.table('rawpackets').filter({ 'processed': 0 }).
-        orderBy('received').limit(4000).run(conn, function (err, cursor) {
-            if (err) throw err;
-            cursor.toArray().then(function (results) {
-                for (var i = 0; i < results.length; i++) {
-                    console.log('parsing object ', i);
-                    var parsedObject = parser.parseDataChunk(results[i], conn);
-
-                    if (parsedObject.batchid > 0) {
-                        if (parsedObject.detectoruid != 74567) {
-                            if (parsedObject == null) {
-                                console.log("Unknown object");
+    rethink.connect({ host: 's7.slashdit.com', port: 28015 }, function (err, conn) {
+        if (err) throw err;
+        conn.use('lightning');
+        rethink.table('rawpackets')
+            .run(conn, function (err, cursor) {
+                if (err) throw err;
+                cursor.toArray().then(function (results) {
+                    for (var i = 0; i < results.length; i++) {
+                        console.log('parsing object ', i);
+                        var newobject = parser.parseDataChunk(results[i], conn);
+                        if (valid(newobject)) {
+                            newobject.persistedDate = Date.now();
+                            if (newobject.packettype != "sample" && newobject.packettype != 0) {
+                                storeStatusPacketInMysql(newobject);
                             }
                             else {
-
-                                if (parsedObject) {
-                                    parsedObject.persistedDate = Date.now();
-                                    if (parsedObject.packettype != "sample" && parsedObject.packettype != 0) {
-
-                                        rethink.db('lightning').table('statuspackets').insert(parsedObject).run(conn, { durability: 'hard' }, function (err, res) {
-                                            if (err) throw err;
-
-                                            console.log('written status packet ', res);
-                                        });
-                                    }
-                                    else {
-                                        if (parsedObject.maxval < 4096) {
-                                            rethink.db('lightning').table('datapackets').insert(parsedObject).run(conn, { durability: 'hard' }, function (err, res) {
-                                                if (err) throw err;
-                                                console.log('written data packet', res);
-
-                                            });
-                                        }
-                                        else {
-                                            console.log('crap packet');
-                                        }
-                                    }
-                                }
+                                storeDataPacketInMysql(newobject);
                             }
                         }
                     }
-                    rethink.table('rawpackets').get(results[i].id).update({ processed: 1 }).run(conn);
-                }
+                });
             });
-        });
+    });
 });
+
+/* helper functions */
+var clean = function (val) {
+    if (val = 'undefined') return null;
+    return val;
+}
+
+var valid = function (obj)
+{
+    return (obj != null);
+}
+
+var storeStatusPacketInMysql = function (parsedObject) {
+
+
+    var sql = "insert into lightning.statuspackets (address, avgadcnoise, batchid, clocktrim, detectoruid, gpsday, gpsfixtype, gpsflags, gpsgspeed, gpshacc, gpshmsl, gpsheading, gpsheadingacc, gpsheight, gpshour, gpsitow, gpslat, gpslon, gpsmin, gpsmonth, gpsnano, gpsnumsv, gpspdop, gpsres1, gpsres2, gpsres3, gpssacc, gpssec, gpstacc, gpsvacc, gpsvalid, gpsveld, gpsvele, gpsveln, gpsyear, gpsuptime, majorversion, minorversion, netuptime, packetnumber, packetssent, packettype, persisteddate, received, sysuptime, triggernoise, triggeroffset, version)" +
+        "VALUES ('" + parsedObject.address + "'," + parsedObject.avgadcnoise + "," + parsedObject.batchid + "," + parsedObject.clocktrim + "," + parsedObject.detectoruid
+        + "," + parsedObject.gps.day
+        + "," + parsedObject.gps.fixType
+        + "," + parsedObject.gps.flags
+        + "," + parsedObject.gps.gSpeed
+        + "," + parsedObject.gps.hAcc
+        + "," + parsedObject.gps.hMSL
+        + "," + parsedObject.gps.heading
+        + "," + parsedObject.gps.headingAcc
+        + "," + parsedObject.gps.height
+        + "," + parsedObject.gps.hour
+        + "," + parsedObject.gps.iTOW
+        + "," + parsedObject.gps.lat
+        + "," + parsedObject.gps.lon
+        + "," + parsedObject.gps.min
+        + "," + parsedObject.gps.month
+        + "," + parsedObject.gps.nano
+        + "," + parsedObject.gps.numSV
+        + "," + parsedObject.gps.pDOP
+        + "," + parsedObject.gps.reserved1
+        + "," + parsedObject.gps.reserved2
+        + "," + parsedObject.gps.reserved3
+        + "," + parsedObject.gps.sAcc
+        + "," + parsedObject.gps.sec
+        + "," + parsedObject.gps.tAcc
+        + "," + parsedObject.gps.vAcc
+        + "," + parsedObject.gps.valid
+        + "," + parsedObject.gps.velD
+        + "," + parsedObject.gps.velE
+        + "," + parsedObject.gps.velN
+        + "," + parsedObject.gps.year + "," + parsedObject.gpsuptime + "," + parsedObject.majorversion + "," + parsedObject.minorversion + "," + parsedObject.netuptime + "," + parsedObject.packetnumber + "," + parsedObject.packetssent + "," + parsedObject.packettype + "," + clean(parsedObject.persisteddate) + "," + parsedObject.received + "," + parsedObject.sysuptime + "," + parsedObject.triggernoise + "," + parsedObject.triggeroffset + "," + parsedObject.version + ")";
+
+    con.execute(sql, function (err, statement) {
+        if (err) {
+            throw err;
+        }
+        console.log('status inserted');
+    });
+}
+
+var storeDataPacketInMysql = function (parsedObject) {
+    if (parsedObject.maxval < 4096) {
+
+        var sql = "INSERT INTO lightning.datapackets(adcseq, address, batchid /*, clocktrim */, data, detectoruid, dmatime/*, firstsampletimestamp, status_fk, rawpacketid*/, maxval, mean, needsprocessing, packetnumber, packettype, persisteddate, received, rtsecs, signaldata, signalcnt, stddev, udpnumber, variance, version ) VALUES (" + parsedObject.adcseq + ",'" + parsedObject.address + "'," + parsedObject.batchid + ",?," + parsedObject.detectoruid + "," + parsedObject.dmatime + "," + parsedObject.maxval + "," + parsedObject.mean + "," + parsedObject.needsprocessing + "," + parsedObject.packetnumber + "," + parsedObject.packettype + "," + clean(parsedObject.persisteddate) + "," + parsedObject.received + "," + parsedObject.rtsecs + ",?," + parsedObject.signalcnt + "," + parsedObject.stddev + "," + parsedObject.udpnumber + "," + parsedObject.variance + "," + clean(parsedObject.version) + ")";
+        con.prepare(sql, function (err, statement) {
+            if (err) {
+                throw err;
+            }
+            // statement.parameters - array of column definitions, length === number of params, here 2
+            // statement.columns - array of result column definitions. Can be empty if result schema is dynamic / not known
+            // statement.id
+            // statement.query
+
+            if (parsedObject.data == undefined || parsedObject.signal == undefined) {
+                // no valid data, 
+                console.log("skipping invalid data / signal");
+
+            }
+            else {
+                statement.execute([parsedObject.data, parsedObject.signal], function (err, rows, columns) {
+                    if (err) {
+                        throw err;
+                    }
+                    console.log("inserted!!!");
+                });
+            }
+            // note that there is no callback here. There is no statement close ack at protocol level.
+            statement.close();
+
+        });
+
+    }
+}
+
--- LightningServer/server.js
+++ LightningServer/server.js
@@ -4,59 +4,58 @@ var host = "0.0.0.0";
 var rethink = require('rethinkdb');
 var net = require("net");
 var dataParser = require("./parse");
+var Fili = require('fili');
 var dgram = require("dgram");
 var stmserver = dgram.createSocket("udp4");
 var express = require('express'),
-   restapiapp = express(),
-   port = 8080,
-   bodyParser = require('body-parser');
+    restapiapp = express(),
+    port = 8080,
+    bodyParser = require('body-parser');
 
 // restapiapp is for web service on port 8080, only / (or index.ejs file at the moment) 
 restapiapp.use(bodyParser.urlencoded({ extended: true }));
 restapiapp.use(bodyParser.json());
 restapiapp.set('view engine', 'ejs');
 restapiapp.get('/', function (req, res) {
-   var connection = null;
-   rethink.connect({ host: 's7.slashdit.com', port: 28015 }, function (err, conn) {
-      if (err) throw err;
-      connection = conn;
+    var connection = null;
+    rethink.connect({ host: 's7.slashdit.com', port: 28015 }, function (err, conn) {
+        if (err) throw err;
+        connection = conn;
 
-      rethink.db('lightning').table('datapackets').orderBy(rethink.desc('received'))
+        rethink.db('lightning').table('datapackets').orderBy(rethink.desc('received'))
 
-         .limit(7).run(connection, function (err, cursor) {
-            if (err) throw err;
-            console.log(cursor);
-            cursor.toArray().then(function (results) {
-               res.render('index', { samples: results });
+            .limit(7).run(connection, function (err, cursor) {
+                if (err) throw err;
+                console.log(cursor);
+                cursor.toArray().then(function (results) {
+                    res.render('index', { samples: results });
+                });
             });
-         });
-   });
+    });
 });
 
-restapiapp.get('/packets/:page', function(req, res, next)
-{
-  var perPage = 8;
-  var page = req.params.page || 1;
-  
-   var connection = null;
-   rethink.connect({ host: 's7.slashdit.com', port: 28015 }, function (err, conn) {
-      if (err) throw err;
-      connection = conn;
+restapiapp.get('/packets/:page', function (req, res, next) {
+    var perPage = 8;
+    var page = req.params.page || 1;
 
-      rethink.db('lightning').table('datapackets').orderBy(rethink.desc('received')).skip((perPage * page) - perPage)
-      .limit(perPage).run(connection, function (err, cursor) {
-            if (err) throw err;
-            console.log(cursor);
-            cursor.toArray().then(function (results) {
-               rethink.db('lightning').table('datapackets').count().run(connection, function( err, count)
-               {
-                  if(err) throw err;
-                  
-                    res.render('packets', { samples: results, current: page, pages: count/perPage });
-               });
+    var connection = null;
+    rethink.connect({ host: 's7.slashdit.com', port: 28015 }, function (err, conn) {
+        if (err) throw err;
+        connection = conn;
+
+        rethink.db('lightning').table('datapackets').orderBy(rethink.desc('received')).skip((perPage * page) - perPage)
+            .limit(perPage).run(connection, function (err, cursor) {
+                if (err) throw err;
+                console.log(cursor);
+                cursor.toArray().then(function (results) {
+                    rethink.db('lightning').table('datapackets').count().run(connection, function (err, count) {
+                        if (err) throw err;
+
+                        res.render('packets', { samples: results, current: page, pages: count / perPage });
+                    });
+                });
             });
-         });
-   });
+    });
 });
 
 
@@ -67,45 +66,112 @@ var groupBy = function (xs, key) {
     }, {});
 };
 
-restapiapp.get('/timeline', function(req, res, next)
-{
+restapiapp.get('/timeline', function (req, res, next) {
 
 
-   var connection = null;
+    var connection = null;
 
-   rethink.connect({ host: 's7.slashdit.com', port: 28015 }, function (err, conn) {
-      if (err) throw err;
-      connection = conn;
+    rethink.connect({ host: 's7.slashdit.com', port: 28015 }, function (err, conn) {
+        if (err) throw err;
+        connection = conn;
 
-      rethink.db('lightning').table('datapackets').hasFields('gps').pluck('gps','detectoruid').run(connection, function (err, cursor) {
+        rethink.db('lightning').table('datapackets').hasFields('gps').pluck('gps', 'detectoruid').run(connection, function (err, cursor) {
             if (err) throw err;
-          cursor.toArray().then(function (results) {
+            cursor.toArray().then(function (results) {
 
-var set =results.map(item => item.detectoruid).filter((value, index, self) => self.indexOf(value) === index);
+                var set = results.map(item => item.detectoruid).filter((value, index, self) => self.indexOf(value) === index);
 
 
-              res.render('timeline', { samples: results, detectors: set });
-               });  // 
-      });  // db
-      
-   });// connect
+                res.render('timeline', { samples: results, detectors: set });
+            });  // 
+        });  // db
+
+    });// connect
 
 
 
 });
 
 restapiapp.get('/signal', function (req, res) {
-   var connection = null;
-   rethink.connect({ host: 's7.slashdit.com', port: 28015 }, function (err, conn) {
-      if (err) throw err;
-      rethink.db('lightning').table('datapackets').filter(rethink.row("signalcnt").gt(0)). orderBy(rethink.desc('received')).limit(1).run(conn, function (err, cursor) {
-         if (err) throw err;
-         console.log(cursor);
-         cursor.toArray().then(function (results) {
-               res.render('signal', { samples: results });
-         });
-      });
-   });
+    var connection = null;
+    rethink.connect({ host: 's7.slashdit.com', port: 28015 }, function (err, conn) {
+        if (err) throw err;
+        rethink.db('lightning').table('datapackets').filter(rethink.row("signalcnt").gt(0)).orderBy(rethink.desc('received')).limit(1).run(conn, function (err, cursor) {
+            if (err) throw err;
+            console.log(cursor);
+            cursor.toArray().then(function (results) {
+                res.render('signal', { samples: results });
+            });
+        });
+    });
+});
+
+restapiapp.get('/filter', function (req, res) {
+    var connection = null;
+    rethink.connect({ host: 's7.slashdit.com', port: 28015 }, function (err, conn) {
+        if (err) throw err;
+        rethink.db('lightning').table('datapackets').filter(rethink.row("signalcnt").gt(0)).orderBy(rethink.desc('received')).limit(1).run(conn, function (err, cursor) {
+            if (err) throw err;
+            console.log(cursor);
+            cursor.toArray().then(function (results) {
+
+                var iirCalculator = new Fili.CalcCascades();
+                var iirFilterCoeffs = iirCalculator.highpass({
+                    order: 1, // cascade 3 biquad filters (max: 12)
+                    characteristic: 'butterworth',
+                    Fs: 2680000, // sampling frequency
+                    Fc: 100, // cutoff frequency / center frequency for bandpass, bandstop, peak
+                    gain: 0, // gain for peak, lowshelf and highshelf
+                    preGain: false // adds one constant multiplication for highpass and lowpass
+                    // k = (1 + cos(omega)) * 0.5 / k = 1 with preGain == false
+                });
+
+                // pauls dodgy filter
+
+                var filterloop = function (a) {
+                    var NZEROS = 4;
+                    var NPOLES = 4;
+                    var GAIN = 0.8;//             1.000306367e+00;
+
+                    var xv = new Array(NZEROS + 1);
+                    var yv = new Array(NPOLES + 1);
+
+                    xv[0] = 0;
+                    xv[1] = 0;
+                    xv[2] = 0;
+                    xv[3] = 0;
+                    xv[4] = 0;
+
+                    yv[0] = 0;
+                    yv[1] = 0;
+                    yv[2] = 0;
+                    yv[3] = 0;
+                    yv[4] = 0;
+
+                    var out= new Array(a.length);
+                    for (var i = 0; i < a.length; i++) {
+                        xv[0] = xv[1];
+                        xv[1] = xv[2];
+                        xv[2] = xv[3];
+                        xv[3] = xv[4];
+
+                        xv[4] = a[i] / GAIN;
+                        yv[0] = yv[1];
+                        yv[1] = yv[2];
+                        yv[2] = yv[3];
+                        yv[3] = yv[4];
+                        yv[4] = (xv[0] + xv[4]) - 4 * (xv[1] + xv[3]) + 6 * xv[2] + (-0.9993875475 * yv[0]) + (3.9981624550 * yv[1]) + (-5.9981622674 * yv[2]) + (3.9993873599 * yv[3]);
+                        out[i] = yv[4];
+
+                    }
+                    return out;
+                }
+                var filter = filterloop(results[0].data);
+                res.render('filter', { samples: results, filter: filter });
+
+            });
+        });
+    });
 });
 
 
@@ -118,67 +184,67 @@ restapiapp.listen(port);
 console.log('REST API server started on: ' + port);
 
 stmserver.on("listening",
-   function () {
-      var address = stmserver.address();
-      console.log("UDP Server listening on " + address.address + ":" + address.port);
-   });
+    function () {
+        var address = stmserver.address();
+        console.log("UDP Server listening on " + address.address + ":" + address.port);
+    });
 
 stmserver.on("message",
-   function (message, remote) {
-      console.log(remote.address + ":" + remote.port);
-      var now = Date.now();
+    function (message, remote) {
+        console.log(remote.address + ":" + remote.port);
+        var now = Date.now();
 
-      // forward to Bob's Boing Machine
-      var client = dgram.createSocket('udp4');
-      client.send(message, 0, message.length, 5001, 'gate.wzone.co.uk', (err) => {
-         if (err) {
-            console.log(err);
-         }
-         else {
-            console.log('packet forwarded');
-         }
-         client.close();
-      });
+        // forward to Bob's Boing Machine
+        var client = dgram.createSocket('udp4');
+        client.send(message, 0, message.length, 5001, 'gate.wzone.co.uk', (err) => {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                console.log('packet forwarded');
+            }
+            client.close();
+        });
 
-      var packet = {
-         version: "0.4",
-         type: "data",
-         data: message,
-         address: remote.address,
-         port: remote.port,
-          timestamp: now,
-         processed: 0,
-         received: new Date().toString()
-         
-      };
+        var packet = {
+            version: "0.4",
+            type: "data",
+            data: message,
+            address: remote.address,
+            port: remote.port,
+            timestamp: now,
+            processed: 0,
+            received: new Date().toString()
+
+        };
 
 
-      rethink.connect({ host: 's7.slashdit.com', port: 28015 }, function (err, conn) {
-         if (err) throw err;
-
-         rethink.db('lightning').table('rawpackets').insert(packet).run(conn, function (err, res) {
+        rethink.connect({ host: 's7.slashdit.com', port: 28015 }, function (err, conn) {
             if (err) throw err;
 
-            console.log("parsing object at " + packet.received);
-            var parsedObject = dataParser.parseDataChunk(packet,conn);
-            if (parsedObject == null) {
-               console.log("Unknown object");
-            } else {
+            rethink.db('lightning').table('rawpackets').insert(packet).run(conn, function (err, res) {
+                if (err) throw err;
 
-               if (parsedObject) {
-                  parsedObject.persistedDate = Date.now();
-                  if (parsedObject.packettype != "sample" && parsedObject.packettype != 0) {
+                console.log("parsing object at " + packet.received);
+                var parsedObject = dataParser.parseDataChunk(packet, conn);
+                if (parsedObject == null) {
+                    console.log("Unknown object");
+                } else {
 
-                     rethink.db('lightning').table('statuspackets').insert(parsedObject).run(conn, function (err, res) { if (err) throw err; });
-                  }
-                  else {
-                     if (parsedObject.maxval < 4096)
-                        rethink.db('lightning').table('datapackets').insert(parsedObject).run(conn, function (err, res) { if (err) throw err; });
-                  }
-               }
-            }
-         });
-      });
-   });
+                    if (parsedObject) {
+                        parsedObject.persistedDate = Date.now();
+                        if (parsedObject.packettype != "sample" && parsedObject.packettype != 0) {
+
+                            rethink.db('lightning').table('statuspackets').insert(parsedObject).run(conn, function (err, res) { if (err) throw err; });
+                        }
+                        else {
+                            if (parsedObject.maxval < 4096)
+                                rethink.db('lightning').table('datapackets').insert(parsedObject).run(conn, function (err, res) { if (err) throw err; });
+                        }
+                    }
+                }
+            });
+        });
+    });
 
 stmserver.bind(stmport, host);
