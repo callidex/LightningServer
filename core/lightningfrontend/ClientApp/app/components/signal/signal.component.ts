932378b51037f89470ca7855e6d8735242d86a83
callidex <paul@callidex.com>
2018-05-29 22:21:07 +1000

    signal changes

--- core/lightningfrontend/ClientApp/app/components/signal/signal.component.ts
+++ core/lightningfrontend/ClientApp/app/components/signal/signal.component.ts
@@ -7,6 +7,38 @@ import { Component } from '@angular/core';
 })
 export class SignalComponent {
 
-    page = 4;
+    chart = []; // This will hold our chart info
 
+
+       this.chart = new Chart('canvas', {
+    type: 'line',
+    data: {
+        labels: weatherDates,
+        datasets: [
+            {
+                data: temp_max,
+                borderColor: "#3cba9f",
+                fill: false
+            },
+            {
+                data: temp_min,
+                borderColor: "#ffcc00",
+                fill: false
+            },
+        ]
+    },
+    options: {
+        legend: {
+            display: false
+        },
+        scales: {
+            xAxes: [{
+                display: true
+            }],
+            yAxes: [{
+                display: true
+            }],
+        }
+    }
+});
 }
