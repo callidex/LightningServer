5b6033779c6dd8efb7b83e9cc48249e8afcc1a7c
callidex <paul@callidex.com>
2018-05-30 19:27:59 +1000

    use interfaces/ geolocation

--- core/lightningfrontend/ClientApp/app/components/detectormap/detectormap.component.ts
+++ core/lightningfrontend/ClientApp/app/components/detectormap/detectormap.component.ts
@@ -1,34 +1,44 @@
 ﻿import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { GeoLocationService  } from '../../geo-location.service';
+
 @Component({
     selector: 'detectormap',
     templateUrl: './detectormap.component.html',
     styleUrls: ['./detectormap.component.css']
 })
-
 export class DetectorMapComponent {
     title: string = 'Detectors';
 
-    public detectors: Detector[] | undefined;
+    public detectors: IDetector[] | undefined;
+    homeDetector: HomeDetector | undefined;
 
-    lat: number = 0;
-    lon: number = 0;
-    label: string = "";
+    constructor(httpClient: HttpClient, private  geoLocationService: GeoLocationService, @Inject('BASE_URL') baseUrl: string) {
 
-
-    constructor(httpClient: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-        httpClient.get<Detector[]>(baseUrl + 'api/SampleData/Detectors').subscribe(result => {
+     
+        httpClient.get<IDetector[]>(baseUrl + 'api/SampleData/Detectors').subscribe(result => {
             this.detectors = result;
-            if (this.detectors.length > 0) {
-                this.lat = this.detectors[0].Lat;
-                this.lon = this.detectors[0].Lon;
-                this.label = this.detectors[0].Name;
+            
+            navigator.geolocation.getCurrentPosition((position) => {
+                this.homeDetector = new HomeDetector();
+                this.homeDetector.Lat = (position.coords.latitude);
+                this.homeDetector.Lon = (position.coords.longitude);
             }
+
+            );
+
+
         }, error => console.error(error));
     }
 }
+class HomeDetector implements IDetector  {
+    Name: string = "You are here";
+    Lat: number = 0;
+    Lon: number = 0;
+    Received: number = 0;
+}
 
-interface Detector {
+interface IDetector {
     Name: string;
     Lat: number;
     Lon: number;
---
+++ core/lightningfrontend/ClientApp/app/geo-location.service.ts
@@ -0,0 +1,14 @@
+﻿import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class GeoLocationService {
+
+    coordinates: any;
+
+    constructor() { }
+
+
+}
