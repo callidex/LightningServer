42d6650ab5e10aa75d58a06c928f93f939f30518
callidex <paul@callidex.com>
2018-06-26 21:32:28 +1000

    sig

--- core/lightningfrontend/ClientApp/app/components/signal/signal.component.ts
+++ core/lightningfrontend/ClientApp/app/components/signal/signal.component.ts
@@ -1,5 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map';
+import { Chart, ChartData, Point } from 'chart.js';
 
 @Component({
     selector: 'app-signal',
@@ -9,12 +11,56 @@ import { HttpClient } from '@angular/common/http';
 
 export class SignalComponent {
 
-    public signals: ISignal[] | undefined;
+    public chart: Chart = new Chart('canvas', {});
+
+    public labels: string[] = new Array();
+    public signals: number[][] | undefined;
 
     constructor(httpClient: HttpClient, @Inject('BASE_URL') baseUrl: string) {
 
-        httpClient.get<ISignal[]>(baseUrl + 'api/SampleData/Signals').subscribe(result => {
+        httpClient.get<number[][]>(baseUrl + 'api/SampleData/Signals').subscribe(result => {
             this.signals = result;
+            var i: number = 0;
+            console.log(this.signals);
+            this.signals[0].forEach(() => { this.labels.push(i.toString()); i++; });
+
+            this.chart = new Chart('canvas', {
+                type: 'line',
+                data: {
+                    labels: this.labels,
+                    datasets: [
+                        {
+                            data: this.signals[0],
+                            borderColor: "#3cba9f",
+                            fill: false
+                        }
+                        //    {
+                        //        data: this.signals,
+                        //        borderColor: "#ffcc00",
+                        //        fill: false
+                        //    },
+                    ]
+                },
+                options: {
+                    legend: {
+                        display: false
+                    },
+                    scales: {
+                        xAxes: [{
+                            display: true
+
+                        }],
+                        yAxes: [{
+                            display: true
+                            , ticks:
+                                {
+                                    min: 0, max: 4000
+                                }
+
+                        }],
+                    }
+                }
+            });
         });
     }
 }
