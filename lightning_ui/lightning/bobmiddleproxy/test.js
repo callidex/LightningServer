111b7a19efbbb903c18714882d341900ac46fb8b
Callidex <paul@callidex.com>
2020-04-08 23:21:00 +1000

    redo proxy

--- lightning_ui/lightning/bobmiddleproxy/test.js
+++ lightning_ui/lightning/bobmiddleproxy/test.js
@@ -1,26 +1,37 @@
-require('rootpath')();
-const request = require('request');
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cors());
+var express = require('express'),
+    request = require('request'),
+    bodyParser = require('body-parser'),
+    app = express();
 
+var myLimit = typeof(process.argv[2]) != 'undefined' ? process.argv[2] : '100kb';
+console.log('Using limit: ', myLimit);
 
-// start node proxy
+app.use(bodyParser.json({limit: myLimit}));
 
-var nodeproxy = express();
-nodeproxy.use('/', function (req, res) {
-	var url = 'http://localhost:9080' + req.url;
-	console.log('Forwarded to ' + url);
-	req.on('error', (error) => { console.log(error) }).pipe(request(url)).on('error', (error) => { console.log(error) }).pipe(res);
+app.all('*', function (req, res, next) {
 
+    // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
+
+    if (req.method === 'OPTIONS') {
+        // CORS Preflight
+        res.send();
+    } else {
+        var targetURL = 'http://b1.vk4ya.space:9080';
+        request({ url: targetURL + req.url, method: req.method, json: req.body, headers: {'Authorization': req.header('Authorization')} },
+            function (error, response, body) {
+                if (error) {
+                    console.error('error: ' + response.statusCode)
+                }
+//                console.log(body);
+            }).pipe(res);
+    }
 });
 
-nodeproxy.listen(3001);
+app.set('port', process.env.PORT || 3000);
 
-
-
-
+app.listen(app.get('port'), function () {
+    console.log('Proxy server listening on port ' + app.get('port'));
+});
