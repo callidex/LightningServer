b038e0af237a0e874c3c2e222f8d50cff3a9a4c0
callidex <paul@callidex.com>
2020-04-12 14:17:39 +1000

    added strike model

---
+++ lightning_ui/lightning/src/app/strike.model.spec.ts
@@ -0,0 +1,7 @@
+import { Strike } from './strike.model';
+
+describe('Strike', () => {
+  it('should create an instance', () => {
+    expect(new Strike()).toBeTruthy();
+  });
+});
---
+++ lightning_ui/lightning/src/app/strike.model.ts
@@ -0,0 +1,7 @@
+export class Strike {
+    lon: any;
+    lat: any;
+    heat: any;
+    dcount: any;
+    stamp: any;
+}
