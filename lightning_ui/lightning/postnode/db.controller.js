7ea7026d67e2b2af056a862d41ebaca867d9081e
callidex <paul@callidex.com>
2022-12-27 16:14:20 +1000

    update

--- lightning_ui/lightning/postnode/db.controller.js
+++ lightning_ui/lightning/postnode/db.controller.js
@@ -3,26 +3,17 @@ const express = require("express");
 const router = express.Router();
 const config = require("./config.json");
 const { Client } = require('pg');
-
 //import geohash from 'latlon-geohash';
-
-
+const io = require('@pm2/io');
+const strikeCounter = io.counter({   name: 'LightSrv strike count',   id: 'app/realtime/strike' });
+const statusCounter = io.counter({   name: 'LightSrv status count',   id: 'app/realtime/status' });
 const { Pool } = require('pg')
-const pool = new Pool({
-          user: config.user,
-          host: config.host,
-          database: config.database,
-          password: config.password,
-          port: config.port,
+const pool = new Pool({           user: config.user,           host: config.host,           database: config.database,           password: config.password,           port: config.port,  
 
-
- max: 300,
+   max: 300,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
           });
-
-
-
 
 // routes
 router.post("/status", status);
@@ -31,67 +22,56 @@ router.post("/strike", strike);
 module.exports = router;
 
 function strike(req, res, next) {
-  console.log(req.body);
+   strikeCounter.inc();
+
   const incomingStrike = req.body;
 ;(async function() {
   const client = await pool.connect()
-  
        const sql = "insert into strikes_hyper(stamp,linecount,heat,hity,hitx,longitude,latitude)  values ("
-+ "TO_TIMESTAMP('" + incomingStrike.stamp + "','dd.mm.YY HH24:MI:SS:MS') - (10 * interval  '1 hour'),"
-  + incomingStrike.linecount + ","
-  + incomingStrike.heat + ","
- +  incomingStrike.hity + ","
- +  incomingStrike.hitx + ","
- +  incomingStrike.lon + ","
- + incomingStrike.lat + ") RETURNING id;";
-       console.log(sql);
-
-
-const result =  await client.query(sql);
-
-const dlist = incomingStrike.dlist.split(",");
-for (var dec in dlist)
-{
-    const dec_id = dlist[dec].split("<")[0];
-    const insert = "insert into strike_detector(strike_id,detector_id) values (" + result.rows[0].id + "," + dec_id + ");";
-    await client.query(insert);
-}
-
-console.log('done')
-       client.release()
-    })()
+      + "TO_TIMESTAMP('" + incomingStrike.stamp + "','dd.mm.YY HH24:MI:SS:MS') - (10 * interval  '1 hour'),"
+      + incomingStrike.linecount + ","
+      + incomingStrike.heat + ","
+      +  incomingStrike.hity + ","
+      +  incomingStrike.hitx + ","
+      +  incomingStrike.lon + ","
+      + incomingStrike.lat + ") RETURNING id;";
+  console.log(sql);
+  try
+  {
+   const result =  await client.query(sql);
+   const dlist = incomingStrike.dlist.split(",");
+   for (var dec in dlist)
+   {
+      const dec_id = dlist[dec].split("<")[0];
+      const insert = "insert into strike_detector(strike_id,detector_id) values (" + result.rows[0].id + "," + dec_id + ");";
+      await client.query(insert);
+   }
+   console.log('done')
+  }
+  finally
+  {
+    client.release()
+  }  
+ })()
     res.status(200).end();
 }
 
 function status(req, res, next) {
-  console.log(req.body);
+  statusCounter.inc();
   const incomingStatus = req.body;
-  console.log(incomingStatus.LastSeen);
-//  if(incomingStatus.clktrim > 109000000 || incomingStatus.clktrim < 107000000)
-//{
-//     console.log( 'rejected - ');
-//     console.log(incomingStatus);
-//     res.status(500).end();
-//     return;
-//  } 
-
   ; (async () => {
-    const client = new Client({
-      user: config.user,
-      host: config.host,
-      database: config.database,
-      password: config.password,
-      port: config.port,
-    })
-    await client.connect()
-//    const geo = geohash.encode(incomingStatus.Lat, incomingStatus.Lon);
-    const geo = '';
-    const sql = "insert into status_hyper (stamp,detectorid, Lon,Lat,height,udpcount,clktrim,satellites,temppress,geohash,temp,press,adctrigoff,adcbase,noise,peaklevel,trigcount,pgagain, jabbering, batchid, splatrev, sensetype,adcpktssent,sysuptime,netuptime,gpsuptime,majorversion,minorversion,auxstatus,adcudpover,udpsent,bconf,gpslocked,epochsecs) values ("
+
+
+    const client =  await pool.connect()
+    try
+   {
+     const geo = '';
+     const sql = "insert into status (stamp,detectorid, Lon,Lat,udpcount,clktrim,satellites,temppress,geohash,temp,press,adctrigoff,adcbase,noise,peaklevel,trigcount,pgagain, jabbering, batchid, splatrev, sensetype,adcpktssent,sysuptime,netuptime,gpsuptime,majorversion,minorversion,auxstatus,adcudpover,udpsent,bconf,gpslocked,epochsecs) values ("
       + "TO_TIMESTAMP('" + incomingStatus.LastSeen + "','dd.mm.YY HH24:MI:SS:MS') - (10 * interval '1 hour'),"
       + incomingStatus.id + ","
       + incomingStatus.Lon + ","
       + incomingStatus.Lat + ","
-      + incomingStatus.Height + ","
+//      + incomingStatus.Height + ","
       + incomingStatus.udpcount + ","
       + incomingStatus.clktrim + ","
       + incomingStatus.satellites + ","
@@ -121,9 +101,12 @@ function status(req, res, next) {
       + incomingStatus.bconf+ ","
       + incomingStatus.gpslocked + ","
       + incomingStatus.epochsecs+ " );";
-      console.log(sql);
+console.log(sql);
       await client.query(sql);
-client .end()
-  })()
+}
+finally
+{
+   client .release()
+}  })()
   res.status(200).end();
 }
--- lightning_ui/lightning/postnode/postgresapi.js
+++ lightning_ui/lightning/postnode/postgresapi.js
@@ -3,22 +3,15 @@ const app = express();
 const errorHandler = require("./errorhandler");
 const bodyParser = require("body-parser");
 
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 
 app.use("/db", require("./db.controller"));
 app.use(errorHandler);
 
-
 const port = process.env.PORT || 3030;
 
-app.listen(port, function () {
-  log("Server listening on port " + port);
-});
+app.listen(port, function () {   log("Server listening on port " + port); });
 
 
-function log(message) {
-  console.log(new Date() + " : " + message);
-}
+function log(message) {   console.log(new Date() + " : " + message); }
