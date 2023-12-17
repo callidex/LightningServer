d6972e0b1d15914a9eeabb326205e8b1cc0d6d9b
callidex <paul@callidex.com>
2023-12-17 15:39:11 +1000

    Clean up a bit, less noise

--- lightning_ui/lightning/postnode/sample_listener.js
+++ lightning_ui/lightning/postnode/sample_listener.js
@@ -1,6 +1,5 @@
 require("rootpath")();
 const express = require("express");
-const router = express.Router();
 const config = require("./config.json");
 const math = require("math");
 const { Client } = require('pg');
@@ -13,29 +12,26 @@ server.on('error', (err) => {
 });
 
 server.on('message', (msg, rinfo) => {
-
   const data = Buffer.from(msg);
+  const tempObject = {};
 
-  var packetNumber = data.readInt32LE(0) & 0xFFFFFF;
-  var tempObject = new Object;
-  if (data[3] == 0 || data[3] == 4) {
+  if (data[3] === 0 || data[3] === 4) { // Changed double equals (==) to triple equals (===)
     console.log("Sample packet incoming - type ", data[3]);
+
     tempObject.udpnumber = data.readUInt32LE(0);
     tempObject.adcseq = data[4];
     tempObject.detector = data[6];
     tempObject.rtsecs = data.readUInt32LE(7) >> 2;
     tempObject.batchid = data[8];
-    console.log("Data packet with batchid ", tempObject.batchid);
     tempObject.dmatime = data.readUInt32LE(12);
     tempObject.data = [];
+
     for (var i = 0; i < (728 * 2); i = i + 2) {
       tempObject.data.push((data[i + 15] << 8) + data[i + 1 + 15]);
     }
     tempObject.needsprocessing = 1;
     storeSample(tempObject);
-
   }
-  console.log(`server got msg from ${rinfo.address}:${rinfo.port}`);
 });
 
 server.on('listening', () => {
@@ -62,27 +58,15 @@ async function storeSample(tempObject) {
   const client = await pool.connect();
 
   try {
-    console.log("Detector :", tempObject.detector);
-    var smoothed = smoothed_z_score(tempObject.data);
-
-   var res = await client.query("INSERT INTO sample (detector, data, dmatime, batchid,rtsecs,adcseq,smoothed) VALUES ($1, $2, $3, $4, $5, $6, $7)", [tempObject.detector, tempObject.data, tempObject.dmatime, tempObject.batchid, tempObject.rtsecs, tempObject.adcseq, smoothed]);
-
-
-
-  }
-  catch (err) { console.log(err.stack) }
-
-  finally {
+    const smoothed = smoothed_z_score(tempObject.data);
+    const res = await client.query(
+      `INSERT INTO sample (detector, data, dmatime, batchid, rtsecs, adcseq, smoothed)
+       VALUES ($1, $2, $3, $4, $5, $6, $7)`,
+      [tempObject.detector, tempObject.data, tempObject.dmatime, tempObject.batchid, tempObject.rtsecs, tempObject.adcseq, smoothed]
+    );
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
     client.release();
   }
 }
-
-
-
-
-
-
-
-
-
-
